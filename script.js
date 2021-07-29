2b5699a722e7a4fd987e3b66e37706d907a669d3
Dinozavrik35 <irishka-cool35rus@mail.ru>
2021-07-29 16:13:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    document.getElementById("burger_nav").style.transform = "translateX(-143px)";
+    document.getElementById("burger").style.color = "white";
+}
+
+function closeMenu() {
+    document.getElementById("burger_nav").style.transform = "translateX(143px)";
+    document.getElementById("burger").style.color = "#255933";
+}
